b0de2c5f298ca91d429ecc493b396385209a100b
Dasthas <danyagalinskii@mail.ru>
2025-08-19 17:48:32 +0400

    add ISceneEnvironment

--- Assets/Scripts/LifetimeScopes/ProjectLifetimeScope.cs
+++ Assets/Scripts/LifetimeScopes/ProjectLifetimeScope.cs
@@ -1,3 +1,4 @@
+using Components;
 using Services;
 using UnityEngine;
 using VContainer;
@@ -7,10 +8,13 @@ namespace LifetimeScopes
 {
     public class ProjectLifetimeScope : LifetimeScope
     {
+        [SerializeField] private SceneEnvironment _sceneEnvironment;
         [SerializeField] private ServicesTable _servicesTable;
         
         protected override void Configure(IContainerBuilder builder)
         {
+            builder.RegisterInstance(_sceneEnvironment)
+                .As<ISceneEnvironment>();
             Debug.Log("Project LifetimeScope");
             _servicesTable.Configure(builder);
         }
