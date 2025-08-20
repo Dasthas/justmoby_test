5da242ad15fa264fb1a4629e0c4b25ffb809fdf5
Dasthas <danyagalinskii@mail.ru>
2025-08-20 18:28:42 +0400

    update prefabs and scene

--- Assets/Scripts/LifetimeScopes/ProjectLifetimeScope.cs
+++ Assets/Scripts/LifetimeScopes/ProjectLifetimeScope.cs
@@ -1,5 +1,7 @@
 using Components;
+using Components.Scene;
 using Services;
+using UI.Input;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -8,15 +10,19 @@ namespace LifetimeScopes
 {
     public class ProjectLifetimeScope : LifetimeScope
     {
+        [SerializeField] private CameraController _cameraController;
         [SerializeField] private SceneEnvironment _sceneEnvironment;
+        [SerializeField] private InputHandler _inputHandler;
         [SerializeField] private ServicesTable _servicesTable;
-        
+
         protected override void Configure(IContainerBuilder builder)
         {
             builder.RegisterInstance(_sceneEnvironment)
                 .As<ISceneEnvironment>();
-            Debug.Log("Project LifetimeScope");
-            _servicesTable.Configure(builder);
+            builder.RegisterInstance(_cameraController)
+                .As<ICameraController>();
+            _inputHandler.Register(builder);
+            _servicesTable.RegisterServices(builder);
         }
     }
 }
