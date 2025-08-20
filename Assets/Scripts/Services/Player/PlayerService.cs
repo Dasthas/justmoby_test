6bbbd793391008001ff0e7b9dca344b56509167e
Dasthas <danyagalinskii@mail.ru>
2025-08-20 21:38:00 +0400

    connect characteristics to player

--- Assets/Scripts/Services/Player/PlayerService.cs
+++ Assets/Scripts/Services/Player/PlayerService.cs
@@ -3,7 +3,11 @@ using Components;
 using Components.Characters;
 using Components.Scene;
 using Services.Base;
+using Services.Characteristics;
+using Services.Characteristics.Settings;
 using Sirenix.OdinInspector;
+using UI.Input;
+using UniRx;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -17,11 +21,33 @@ namespace Services.Player
         [SerializeField] private float _defaultHealth = 100f;
         [SerializeField] [AssetsOnly] private PlayerProxy _prefab;
 
-        private PlayerProxy _playerProxy;
-        public PlayerProxy PlayerProxy => _playerProxy;
-
         [Inject] private ISceneEnvironment _sceneEnvironment;
         [Inject] private ICameraController _cameraController;
+        [Inject] private ICharacteristicsService _characteristicsService;
+        [Inject] private GameHud _gameHud;
+
+        private PlayerProxy _playerProxy;
+        private CompositeDisposable _disposables;
+
+        PlayerProxy IPlayerService.PlayerProxy => _playerProxy;
+
+        public void OnPlayerHealthChanged(HealthChangedData healthChangedData)
+        {
+            _gameHud.UpdateHealth(healthChangedData.CurrentHealth, healthChangedData.MaxHealth);
+        }
+
+        private void OnCharacteristicsUpdated(CharacteristicType characteristicType)
+        {
+            if (characteristicType != CharacteristicType.Health)
+            {
+                return;
+            }
+
+            _playerProxy.HealthController.SetMaxHealth(
+                _characteristicsService.CalculateUpgradedValue(_defaultHealth, CharacteristicType.Health));
+        }
+
+        #region Service
 
         public override Service RegisterAndGetInstance(IContainerBuilder builder)
         {
@@ -34,16 +60,24 @@ namespace Services.Player
 
         protected override void OnInitialize()
         {
-            Debug.Assert(_sceneEnvironment != null);
+            _disposables = new CompositeDisposable();
             _playerProxy = Object.Instantiate(_prefab, _sceneEnvironment.DynamicObjectsParent);
+            _playerProxy.OnHealthChanged
+                .Subscribe(OnPlayerHealthChanged)
+                .AddTo(_disposables);
             _playerProxy.Initialize(_defaultHealth);
+            _characteristicsService.OnCharacteristicUpgraded
+                .Subscribe(OnCharacteristicsUpdated)
+                .AddTo(_disposables);
+
             _cameraController.SetFollowTarget(_playerProxy.transform);
-            Debug.Log("OnInitialize PlayerService");
         }
 
         protected override void OnDispose()
         {
-            Debug.Log("OnDispose PlayerService");
+            _disposables.Dispose();
         }
+
+        #endregion
     }
 }
